91db9e057817817039144302a932c74f1980fac4
Wojciech Tartanus <wojciech.tartanus@synergycodes.com>
2022-04-05 21:48:46 +0200

    Improved login tests.

--- src/__test__/login.test.tsx
+++ src/__test__/login.test.tsx
@@ -1,34 +1,45 @@
 /**
- * @jest-environment jsdom
- */
- import '@testing-library/jest-dom'
- import { render, screen } from '@testing-library/react';
- import userEvent from '@testing-library/user-event'
- 
- import { fetchSuccess } from '~/__mocks__/fetchSuccess';
- import { fetchUnauthorized } from '~/__mocks__/fetchUnauthorized';
- 
- import App from '../App';
- 
+* @jest-environment jsdom
+*/
+import '@testing-library/jest-dom'
+import { Router } from 'react-router-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import {createMemoryHistory} from 'history'
+import userEvent from '@testing-library/user-event'
+
+import { fetchSuccess } from '~/__mocks__/fetchSuccess';
+import { fetchUnauthorized } from '~/__mocks__/fetchUnauthorized';
+
+import Login from '../components/Login/Login';
+
  describe('Login component', () => {
     test('should redirect to password health after successful login', async () => {
         global.fetch = jest.fn().mockImplementation(fetchSuccess);
-        
-        render(<App />);
+        const history = createMemoryHistory();
+
+        render(
+            <Router history={history}>
+                <Login />
+            </Router>
+        );
 
         userEvent.type(screen.getByPlaceholderText('Username'), 'test');
         userEvent.type(screen.getByPlaceholderText('Password'), 'test');
         userEvent.click(screen.getByRole('button'));
 
-        global.Storage.prototype.getItem = jest.fn().mockImplementationOnce(() => 'token');
-        expect(await screen.findByText('Create new complex passwords to protect your accounts'))
-            .toBeInTheDocument();
+        await waitFor(() => expect(history.location.pathname).toBe('/items'))
     });
 
     test('should show unauthorized error when login failed', async () => {
         global.fetch = jest.fn().mockImplementation(fetchUnauthorized);
         
-        render(<App />);
+        const history = createMemoryHistory();
+        
+        render(
+            <Router history={history}>
+                <Login />
+            </Router>
+        );
 
         userEvent.type(screen.getByPlaceholderText('Username'), 'test');
         userEvent.type(screen.getByPlaceholderText('Password'), 'test');

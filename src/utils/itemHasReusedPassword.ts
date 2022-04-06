73027273060c5875f60a9c2343f8616c9a74c179
Wojciech Tartanus <wojciech.tartanus@synergycodes.com>
2022-04-06 12:31:00 +0200

    Increase required password strenght

--- src/utils/itemHasReusedPassword.ts
+++ src/utils/itemHasReusedPassword.ts
@@ -3,7 +3,7 @@ import {IItem} from "~/services/getUserItems";
 const itemHasReusedPassword = (item: IItem, itemList: Array<IItem>) => {
   const reusedItems = itemList.filter((listItem) => (
     listItem.password === item.password
-  ))
+  ));
 
   return reusedItems.length > 1;
 };
